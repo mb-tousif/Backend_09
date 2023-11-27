e2012794ba346c2245a1c36cfb01876b63fb6029
mb-tousif <meta.block.tousif@gmail.com>
2023-11-27 11:56:28 +0600

    deploy bug solving

--- dist/app/modules/blog/Blogs.constants.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.BlogsFilterAbleField = exports.BlogsSearchAbleField = void 0;
-exports.BlogsSearchAbleField = ["userId", "title"];
-exports.BlogsFilterAbleField = ["search", "userId", "title"];
-//# sourceMappingURL=Blogs.constants.js.map
--- dist/app/modules/payment/Payment.constants.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PaymentFilterAbleField = exports.PaymentSearchAbleField = void 0;
-exports.PaymentSearchAbleField = ["status", "userId", "bookingId", "serviceId", "amount"];
-exports.PaymentFilterAbleField = ["search", "status", "userId", "bookingId", "serviceId", "amount"];
-//# sourceMappingURL=Payment.constants.js.map
