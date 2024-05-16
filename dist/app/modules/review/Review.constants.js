7727b36f45f8c4c038009d3abf0ff42b26424cd0
mb-tousif <meta.block.tousif@gmail.com>
2024-05-16 10:24:11 +0600

    Docker added

--- dist/app/modules/review/Review.constants.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ReviewFilterAbleField = exports.ReviewSearchAbleField = void 0;
-exports.ReviewSearchAbleField = ["userId", "serviceId", "rating"];
-exports.ReviewFilterAbleField = ["search", "userId", "serviceId", "rating"];
-//# sourceMappingURL=Review.constants.js.map
